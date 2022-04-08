d03f826cc50a38cea9a6364752cc6eef7a4d3952
AleksandrTsygulev <aleksandr.tsygulev@gmail.com>
2022-04-08 18:41:46 +0300

    переделал

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -48,9 +48,9 @@ public class Main {
 
         int[] even = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         for (int i = 0; i < even.length; i++) {
-        if (even[i] % 2 != 0)
-        even[i] = even[i] + 1;
-            System.out.println(even[i]);
+            if (even[i] % 2 != 0)
+                even[i] = even[i] + 1;
+                System.out.println(even[i]);
 
 
         }
